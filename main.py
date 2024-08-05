1b21c30b234904655c0c77eb571cc1d0fa82790e
sidouup <160077558+sidouup@users.noreply.github.com>
2024-08-05 04:56:41 -0400

    Update main.py

--- main.py
+++ main.py
@@ -18,6 +18,5 @@ if st.button("Log in", type="primary"):
         st.session_state.logged_in = True
         st.success("Logged in successfully!")
         sleep(0.5)
-        st.switch_page("dashboard")
     else:
         st.error("Incorrect username or password")
